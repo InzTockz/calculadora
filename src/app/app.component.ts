26695bbe72b05853d83fc6939508f59f46ed6338
Fredy <inztockz@gmail.com>
2025-03-15 11:52:41 -0500

    Subiendo mi calculadora - proyecto personal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculadoraComponent } from "./components/calculadora/calculadora.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'calculadora';
 }
